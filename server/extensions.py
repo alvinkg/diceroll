371e362cc1ce0c385466902d7b9e98a528371796
= <=>
2023-05-12 17:15:32 +0800

    added code to make blueprint

--- server/extensions.py
+++ server/extensions.py
@@ -1,7 +1,9 @@
+from flask import Blueprint
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
 # extensions is where we keep all our extensions
+extensions = Blueprint('extensions',__name__)
 
 # instantiate an object of JWTManager
 jwt = JWTManager()
