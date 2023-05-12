bd7f1814cb072c47b72cc2629f714a57aae65b28
= <=>
2023-05-12 17:16:09 +0800

    tidied formatting

--- server/keygen.py
+++ server/keygen.py
@@ -1,7 +1,9 @@
 from secrets import token_urlsafe
 
+# mtd creates a new key when run
 def key():
     print(token_urlsafe(12))
-    
+
+# method 'key' is called when the pgm is run
 if __name__ == "__main__":
     key()
